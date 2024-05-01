56c1bbb529e177e06463a3d1e702290ed040043c
MattDevelop3r <108505567+MattDevelop3r@users.noreply.github.com>
2024-05-01 20:07:25 -0300

    Delete Ejercicio5 directory

--- Ejercicio5/main.py
+++
@@ -1,7 +0,0 @@
-from gestorEquipos import GestorEquipos
-from gestorFechas import GestorFechas
-from claseEquipo import Equipo
-from claseFecha import Fecha
-if __name__ == '__main__':
-    l = GestorEquipos()
-    l.leeCSV()
